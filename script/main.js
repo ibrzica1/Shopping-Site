64db9be0d774284e9099b943d9754a63653cd572
ibrzica1 <168496865+ibrzica1@users.noreply.github.com>
2025-06-19 17:30:52 +0100

    Start to build products grid

---
+++ script/main.js
@@ -0,0 +1,11 @@
+import { products } from "../data/products";
+
+
+const productsGrid = document.getElementById("productsGrid");
+
+function renderProductsGrid(array) {
+  productsGrid.innerHTML = "";
+  array.forEach(product => {
+    
+  })
+}
