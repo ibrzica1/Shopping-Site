dd98d2d5a246a071d2dfb3a132c74c384515d974
ibrzica1 <168496865+ibrzica1@users.noreply.github.com>
2025-06-20 17:48:56 +0100

    Create products grid

--- script/main.js
+++ script/main.js
@@ -1,11 +1,27 @@
-import { products } from "../data/products";
+import { products } from "../data/products.js";
 
 
 const productsGrid = document.getElementById("productsGrid");
 
 function renderProductsGrid(array) {
-  productsGrid.innerHTML = "";
+  let productsGridHTML = "";
   array.forEach(product => {
-    
-  })
+    productsGridHTML += `
+    <div class="product-container">
+      <div class="product-image-container">
+      <img src="${product.image}">
+      </div>
+      <div class="product-name">${product.name}</div>
+      <div class="product-price">$${formatCurrency(product.priceCents)}</div>
+      <button id="js-add-to-cart" data-product-id="${product.id}">Add to Cart</button>
+    </div>
+    `
+  });
+  productsGrid.innerHTML = productsGridHTML;
+}
+
+renderProductsGrid(products);
+
+function formatCurrency(cents){
+  return(Math.round(cents)/100).toFixed(2);
 }
