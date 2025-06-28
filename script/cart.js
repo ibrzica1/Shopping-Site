36e42452bfbfc62300296cae5ae350e4a61a1b76
ibrzica1 <168496865+ibrzica1@users.noreply.github.com>
2025-06-28 17:17:43 +0100

    Display cart products

--- script/cart.js
+++ script/cart.js
@@ -1,7 +1,8 @@
 import { products } from "../data/products.js";
+import { formatCurrency } from "../utils/money.js";
 
 const cartProductsGrid = document.getElementById("cartProductsGrid");
-const cartOrdersGrid = document.getElementById("cartOrdersGrid");
+const cartTotalContainer = document.getElementById("cartTotalContainer");
 const cartCounter = document.getElementById("cartCounter");
 
 
@@ -32,3 +33,24 @@ export function cartProductsCount(array) {
   cartCounter.innerHTML = count;
 }
 cartProductsCount(cart);
+
+function renderCartProductsGrid(array) {
+    cartProductsGrid.innerHTML = "";
+
+
+  array.forEach(product => {
+    const matchingProduct = products.find(item => item.id === product.productId);
+    cartProductsGrid.innerHTML += `
+      <div class="cart-product-container">
+            <div class="cart-product-image-container">
+            <img src="${matchingProduct.image}">
+            </div>
+            <div class="cart-product-name">${matchingProduct.name}</div>
+            <div class="cart-product-price">$${formatCurrency(matchingProduct.priceCents)}</div>
+            <div class="cart-product-quantity">Quantity: ${product.quantity} </div>
+          </div>
+    `;
+  })
+}
+renderCartProductsGrid(cart);
+
